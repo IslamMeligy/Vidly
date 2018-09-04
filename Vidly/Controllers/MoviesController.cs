ca4bb0443fee5cb9dd72ddea4e598122f193980b
Islam Meligy <islammeligy@hotmail.com>
2018-09-04 17:06:29 +0200

    Start_2

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            Movie movie = new Movie() { Name = "Shrek!" };
+
+            return View(movie);
+            //return Content("Hello World");
+            //return HttpNotFound();
+            //return new EmptyResult();
+            //return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name" });
+        }
+
+        public ActionResult Edit(int movieId)
+        {
+            return Content("movieId=" + movieId);
+        }
+    }
+}
