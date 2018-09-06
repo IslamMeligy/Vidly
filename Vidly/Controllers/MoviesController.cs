453a3eba5e4cf820da21b8c8b1cb06b715f10815
Islam Meligy <islammeligy@hotmail.com>
2018-09-06 17:51:26 +0200

    Start

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -1,25 +1,74 @@
-﻿using System.Web.Mvc;
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
 using Vidly.Models;
+using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
         // GET: Movies/Random
+        //Action Results
+        //Passing Data to Views
         public ActionResult Random()
         {
             Movie movie = new Movie() { Name = "Shrek!" };
+            List<Customer> Customers = new List<Customer>
+            {
+                new Customer {Name= "Customer 1"},
+                new Customer {Name= "Customer 2" }
+            };
 
-            return View(movie);
-            //return Content("Hello World");
-            //return HttpNotFound();
-            //return new EmptyResult();
-            //return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name" });
+            RandomMovieViewModel viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = Customers
+            };
+
+            return View(viewModel);
+            #region Old Training Code
+
+            //Passing Data to Views
+            ////ViewData["Movie"] = movie;
+            ////ViewBag.RandomMovie = movie;
+            ////ToCheck///var viewResult = new ViewResult();
+            ////ToCheck///viewResult.ViewData.Model
+
+
+            //ActionResults
+            ////return View(movie);
+            ////return Content("Hello World");
+            ////return HttpNotFound();
+            ////return new EmptyResult();
+            ////return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name" });
+            #endregion
         }
 
-        public ActionResult Edit(int movieId)
-        {
-            return Content("movieId=" + movieId);
-        }
+        #region Old Training Code
+        ////Action Parameters
+        //public ActionResult Edit(int movieId)
+        //{
+        //    return Content("movieId=" + movieId);
+        //}
+
+        ////Action Parameters
+        //public ActionResult Index(int? pageIndex, string sortBy)
+        //{
+        //    if (!pageIndex.HasValue)
+        //        pageIndex = 1;
+        //    if (string.IsNullOrWhiteSpace(sortBy))
+        //        sortBy = "Name";
+
+        //    return Content(string.Format("Page Index = {0} " + System.Environment.NewLine + " Sort By = {1}", pageIndex, sortBy));
+        //}
+
+        ////Attribute Routing
+        //[Route("movies/released/{year}/{month:regex(\\d{1,2}):range(1,12)}")]
+        //public ActionResult ByReleaseDate(int year,int month)
+        //{
+        //    //return View();
+        //    return Content(year + "/" + month);
+        //}
+        #endregion
     }
 }
